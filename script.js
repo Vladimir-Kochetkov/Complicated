7f83ef3c1d16f44a59e5c030ce4934baa44a32a5
Vladimir-Kochetkov <js.kochetkov@gmail.com>
2022-01-28 16:42:53 +0300

    Усложненное заданиие №1

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+let num = 266219;
+let arr = String(num).split('');
+
+let result = arr.reduce((item, current) =>
+    item * current, 1);
+console.log(result);
+
+let degree = result ** 3;
+console.log(degree);
+
+console.log(String(degree).slice(0, 2));
