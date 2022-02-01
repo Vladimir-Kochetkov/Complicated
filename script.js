cf78943a5c0c07975e8d4226ee3e9446b4cbfda2
Vladimir-Kochetkov <js.kochetkov@gmail.com>
2022-02-02 00:04:04 +0300

    add(script.js):complicated 4 task

--- script.js
+++ script.js
@@ -1,32 +1,13 @@
 "use strict";
-let lang = 'en';
-
-if (lang === 'ru') {
-    console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
-} else if (lang === 'en') {
-    console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-}
-
-switch (lang) {
-    case 'ru':
-        console.log("Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье");
-        break;
-    case 'en':
-        console.log("Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday");
-        break;
-}
-
-lang = [
-    ["Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье"],
-    ["Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday"]
-];
-console.log(lang[0]);
-console.log(lang[1]);
-
-
-let namePerson = prompt('Введите имя', 'Артем');
-
-let socialStatus = (namePerson === "Артем") ? 'директор' :
-    (namePerson === "Александр") ? 'преподаватель' :
-        'студент';
-console.log(socialStatus);   
+const message = "   Строчный аргумент";
+const lessonFour = function (variable) {
+    variable.trim();
+    if (typeof variable != "string") {
+        console.log('В качестве аргумента передана не строка');
+    } else if (variable.length < 30) {
+        console.log(variable.trim());
+    } else if (variable.length > 30) {
+        console.log((variable.slice(0, 29)).trim() + '...');
+    }
+};
+lessonFour(message);
